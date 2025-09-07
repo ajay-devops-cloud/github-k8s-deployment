31392c5eff7a368d24bb6dcd373e4c227cbd897f
Ajay Yadav <ajaykumaryadav97@gmail.com>
2025-09-07 16:02:15 +0530

    ttr

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -1,7 +1,7 @@
 terraform {
     backend "azurerm" {
       resource_group_name = "rg-it"
-      storage_account_name = "itsolution1"
+      storage_account_name = "itsolution2"
       container_name = "nextlevelcontainer"
       key = "terraform1.tfstate"
       
